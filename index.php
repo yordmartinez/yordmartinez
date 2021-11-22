a939f7a43b2831637075f51694f696c382c970f8
yordmartinez <94840771+yordmartinez@users.noreply.github.com>
2021-11-22 07:59:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<html>
+  <head>
+    <title> Menz </title>
+  </head>
+  <body>
+    <form method="POST" action="uno.php">
+      Username: <br> <input type="text" name="username"> <br>
+      Password: <br> <input type="password" name="password"> <br>
+      <input type="submit" name="enviar">
+    
+  </body>
+</html>
